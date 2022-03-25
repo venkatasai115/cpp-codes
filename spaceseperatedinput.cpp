8e41f8738d35b25086898b42c4d653ee73231b54
venkatasai115 <86653732+venkatasai115@users.noreply.github.com>
2022-03-25 15:47:12 +0530

    Add files via upload

---
+++ spaceseperatedinput.cpp
@@ -0,0 +1,29 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> split(string s){
+    string temp="";
+    char b=' ';
+    vector<string>v;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=b){
+            temp+=s[i];
+        }
+        else{
+            v.push_back(temp);
+            temp="";
+        }
+    }
+    return v;
+}
+int main()
+{
+    string s;
+    getline(cin,s);
+    vector<string>l=split(s);
+    for(auto x:l){
+        cout<<stoi(x)<<"\n";
+    }
+    return 0;
+}
