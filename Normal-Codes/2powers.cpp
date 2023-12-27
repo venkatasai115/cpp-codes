4f294609305a26d26a61ee2fc8ddf4cf3544a61b
venkatasai115 <venkatasai1152@gmail.com>
2023-12-28 02:09:09 +0530

    changed

--- Normal-Codes/2powers.cpp
+++ Normal-Codes/2powers.cpp
@@ -1,36 +1,36 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-int k=1;
-bool isPower(int x){
-    return(ceil(log2(x))==floor(log2(x)));
+#include <bits/stdc++.h>
+using namespace std;
+int k = 1;
+bool isPower(int x)
+{
+    return (ceil(log2(x)) == floor(log2(x)));
 }
-string rec(int n,vector<string> &v){
-    v[1]="(2^0)";
-    for(int i=2;i<=n;i++)
+string rec(int n, vector<string> &v)
+{
+    v[1] = "(2^0)";
+    for (int i = 2; i <= n; i++)
     {
-    if(isPower(i))
-    {
-       int p=log(i)/log(2);
-       v[i]="(2^("+v[p]+"))";
-       k=i;
-    }
-    else{
-       v[i]=v[k]+"+"+v[i-k];
-    }
+        if (isPower(i))
+        {
+            int p = log(i) / log(2);
+            v[i] = "(2^(" + v[p] + "))";
+            k = i;
+        }
+        else
+        {
+            v[i] = v[k] + "+" + v[i - k];
+        }
     }
     return v[n];
 }
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cin>>n;
-    string s=" ";
-    vector<string>v(n+1,s);
-    cout<<rec(n,v);
+    cin >> n;
+    string s = " ";
+    vector<string> v(n + 1, s);
+    cout << rec(n, v);
     return 0;
 }
-
-
-
-
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cpp-codes
 
-My daily practice codes
+My daily practice cpp codes
+
+In Vscode for running any cpp file use cmd+shift+B and give input in input.txt and see output in output.txt
